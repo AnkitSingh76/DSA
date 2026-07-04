12503448a803a298d92dea2d146cae289628e662
Ankit Singh <125785064+AnkitSingh76@users.noreply.github.com>
2026-07-04 20:29:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.83%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2)
+            return x;
+
+        int ans = x, start = 1, end = x, mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (mid == x / mid) {
+                ans = mid;
+                break;
+            } else if (mid < x / mid) {
+                ans = mid;
+                start = mid + 1;
+            } else
+                end = mid - 1;
+        }
+        return ans;
+    }
+};
